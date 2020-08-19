23bbf65f4f1368afa55e1b18ea84835d95a253df
AechGG <hazg@me.com>
2020-08-19 15:13:01 +0100

    feat: add table view delegate methods to select and deselect items

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -33,6 +33,17 @@ class TodoListViewController: UITableViewController {
         return cell;
     }
 
-
+    // MARK: - UITableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none;
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark;
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true);
+    }
 }
 
