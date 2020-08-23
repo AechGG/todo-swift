0ac81f7fcd429ff3319af848f1079f14ca0f3ca9
AechGG <hazg@me.com>
2020-08-23 16:29:18 +0100

    feat: save data

--- Todo/Controllers/TodoListViewController.swift
+++ Todo/Controllers/TodoListViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]();
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist");
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        loadItems();
+//        loadItems();
     }
     
     // MARK: - UITableView DataSource
@@ -59,8 +61,9 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (alertAction) in
             
-            let newItem = Item()
+            let newItem = Item(context: self.context);
             newItem.title = textField.text!;
+            newItem.done = false;
             
             self.itemArray.append(newItem);
             
@@ -80,31 +83,28 @@ class TodoListViewController: UITableViewController {
     // MARK: - Save Items
     
     func saveItems() {
-        let encoder = PropertyListEncoder();
-        
         do {
-            let data = try encoder.encode(itemArray);
-            try data.write(to: dataFilePath!);
+            try context.save();
         } catch {
-            print("error encoding");
+            print("error saving data \(error)");
         }
         
         self.tableView.reloadData();
     }
     
-    // MARK: - Load Items
-    
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder();
-            
-            do {
-                itemArray = try decoder.decode([Item].self, from: data);
-            } catch {
-                print("Error decoding saved items");
-            }
-            
-        }
-    }
+//    // MARK: - Load Items
+//
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder();
+//
+//            do {
+//                itemArray = try decoder.decode([Item].self, from: data);
+//            } catch {
+//                print("Error decoding saved items");
+//            }
+//
+//        }
+//    }
 }
 
