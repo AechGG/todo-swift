36a0b46a996695cfb0f466ed6869c222e135f414
AechGG <hazg@me.com>
2020-08-26 12:06:18 +0100

    feat: query realm and add new field

--- Todo/Controllers/TodoListViewController.swift
+++ Todo/Controllers/TodoListViewController.swift
@@ -79,6 +79,7 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write {
                         let newItem = Item();
                         newItem.title = textField.text!;
+                        newItem.dateCreated = Date();
                         currentCategory.items.append(newItem);
                     }
                 } catch {
@@ -110,24 +111,22 @@ class TodoListViewController: UITableViewController {
 
 // MARK: - Search Bar Delegate Methods
 
-//extension TodoListViewController: UISearchBarDelegate {
-//
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        let request: NSFetchRequest<Item> = Item.fetchRequest();
-//
-//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)];
-//
-//        loadItems(with: request, predicate: NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!));
-//    }
-//
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        if searchBar.text?.count == 0 {
-//            loadItems();
-//
-//            DispatchQueue.main.async {
-//                searchBar.resignFirstResponder();
-//            }
-//        }
-//    }
-//}
+extension TodoListViewController: UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        items = items?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true);
+        
+        tableView.reloadData();
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems();
+
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder();
+            }
+        }
+    }
+}
 
